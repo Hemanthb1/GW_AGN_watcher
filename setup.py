666ae64af2e9eb79e1e6ba18f885989ea175e480
Hemanth <107019973+Hemanthb1@users.noreply.github.com>
2025-11-25 23:31:06 -0300

    Bump version from 0.2.8 to 0.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gw_agn_watcher",
-    version="0.2.8",
+    version="0.2.9",
     author="Hemanth Kumar",
     author_email="hemanth.bommireddy195@gmail.com",
     description="Python tools for GW AGN follow-up",
